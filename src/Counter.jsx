3a4af956adfe8a860d305f3d7b6511ea80771377
Geethu Mohanan26 <geethumohanmeppurathuthadathi@gmail.com>
2026-05-01 18:25:52 +0530

    first commit

---
+++ src/Counter.jsx
@@ -0,0 +1,93 @@
+// import React,{useState,useEffect} from 'react';
+
+// function Counter() {
+//   const [count,setCount]=useState(0)
+//   useEffect(()=>{
+//     console.log('Mounting...');
+
+//   })
+
+  
+  
+//   return (
+//     <div>
+//       <button onClick={()=>setCount(count+1)}>Increment</button>
+//       <h1>Hello I am component {count}</h1>
+//     </div>
+//   );
+// }
+
+// export default Counter;
+
+// import React,{useState,useEffect} from "react";
+// function Counter(){
+//   const[count,setCount]=useState(0)
+//   useEffect(()=>{
+//     console.log('Mounting');
+//     return()=>{
+//       console.log('Unmounting')
+// });
+//   },[]);
+//   return(
+//     <div>
+//       <button onClick={()=>setCount(count+1)}>Increment</button>
+//       <h1>Hello I am component{count}</h1>
+//     </div>
+//   );
+// }
+// export default Counter;
+
+
+
+
+// import React,{useState,useEffect}  from 'react';
+
+
+
+// function Counter() {
+//   const[count,setCount]=useState(0)
+// const[count2,setCount2]=useState(0)
+  
+//   useEffect(()=>{
+//     console.log('Mounting....');
+//     console.log('Unmounting...');
+//     console.log('Updating...+count');
+//   },[count])
+//   return (
+//     <div>
+//       <button onClick={()=>setCount(count+1)}>Increment</button>
+//       <h1>Hello I am component{count}</h1>
+//       <button onClick={()=>setCount2(count2+1)}>Increment</button>
+//       <h1>Hello I am component2{count2}</h1>
+//     </div>
+//   );
+// }
+
+// export default Counter;
+
+
+
+import React, { useState,useEffect } from 'react'
+
+function Counter() {
+  const[count,setCount]=useState(0)
+  useEffect(()=>{
+    console.log('Mounting...');
+
+//   })
+//   return()=>{
+//     console.log('Unmounting...');
+  
+  
+// },[count];
+}
+  return (
+    <div>
+      <button onClick={()=>setCount(count+1)}>Increment</button>
+      <h1>Hello I am component{count}</h1>
+    </div>
+  );
+}
+
+export default Counter;
+
