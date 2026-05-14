b47bf145e32ac7b9584f138f3e48fddbc6a8f2c6
Geethu Mohanan26 <geethumohanmeppurathuthadathi@gmail.com>
2026-05-14 23:50:17 +0530

    remove comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,3 @@
-// import {useState,useEffect} from 'react';
-// import Counter from './Counter';
-
-
-
-
-// function App() {
-//   const[state,setState]=useState(false)
-  
-    
-  
-
- 
-//   return (
-//     <div className='App'>
-//       <h1 onClick={()=>setState(!state)}>Show/Hide</h1>
-//       {state &&<Counter/>}
-//     </div>
-//   );
-
-
- 
 import {useState} from 'react';
 import "./App.css";
 
